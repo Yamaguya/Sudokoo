2003b1a0a40f9ba08063a112610a7ed94a34f107
Yamaguya <alextzov@outlook.com>
2024-05-15 20:16:31 +0300

    Initial commit

---
+++ app/src/main/java/com/yama/sudokoo/Board.kt
@@ -0,0 +1,64 @@
+package com.yama.sudokoo
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import kotlin.math.min
+
+open class Board(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+
+    private var sqrSize = 3
+    private var size = 9
+    private var cellSizePixels = 0F
+
+    private val thickLinePaint = Paint().apply {
+        style =  Paint.Style.STROKE
+        color = Color.BLACK
+        strokeWidth = 4F
+    }
+
+    private val thinLinePaint = Paint().apply {
+        style =  Paint.Style.STROKE
+        color = Color.BLACK
+        strokeWidth = 2F
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val sizePixels = min(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(sizePixels, sizePixels)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        cellSizePixels = (width / size).toFloat()
+        drawLines(canvas)
+    }
+
+    private fun drawLines(canvas: Canvas) {
+        canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(), thickLinePaint)
+
+        for (i in 1 until size) {
+            val paintToUse = when (i % sqrSize) {
+                0 -> thickLinePaint
+                else -> thinLinePaint
+            }
+            canvas.drawLine(
+                i * cellSizePixels,
+                0F,
+                i * cellSizePixels,
+                height.toFloat(),
+                paintToUse
+            )
+            canvas.drawLine(
+                0F,
+                i * cellSizePixels,
+                width.toFloat(),
+                i * cellSizePixels,
+                paintToUse
+            )
+        }
+    }
+}
